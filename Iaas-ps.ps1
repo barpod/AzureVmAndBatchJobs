77c8f8707d5cf54e5b64f5219585d7d08a0eb29b
Bartosz Podlejski <Bartosz.podlejski@gmail.com>
2020-06-11 19:47:22 +0200

    Add Powershell and AzureCli VM

---
+++ Iaas-ps.ps1
@@ -0,0 +1,25 @@
+﻿Login-AzAccount
+
+Get-AzSubscription
+
+Set-AzContext -SubscriptionName "Azure subscription 1"
+
+$location = "westeurope"
+$rgName = "Iaas-ps-rg"
+$credentials = Get-Credential
+
+New-AzResourceGroup -Name $rgName -Location $location
+
+New-AzVM `
+    -ResourceGroupName $rgName `
+    -Name "Iaas-ps" `
+    -Location $location `
+    -VirtualNetworkName "Iaas-ps-vnet" `
+    -SubnetName "Iaas-ps-subnet" `
+    -PublicIpAddressName "Iaas-ps-ip" `
+    -OpenPorts 80,3389 `
+    -Credential $credentials
+
+Get-AzPublicIpAddress -ResourceGroupName $rgName
+
+Remove-AzResourceGroup -Name $rgName
